734614c3eb0f027570b95bc826671a18ce429d8c
Alexandra Karlsson <betruebefree@gmail.com>
2021-01-16 15:49:52 +0100

    Check if backup directory exists and handle empty image directory.

--- restore.sh
+++ restore.sh
@@ -3,11 +3,19 @@
 
 backup_date=$1
 echo $backup_date
+backup_dir=`backup/$backup_date/`
+echo $backup_dir
 
 # Check that the backup for the date exists otherwise exit
+if [ -d $backup_dir ] 
+then
+    echo `Directory $backup_dir exists.` 
+    echo "Remove all images in images directory."
+    rm -f images/*
 
-# Remove all images in images directory
-rm images/*
-
-# Copy all images from selected backup
-cp backup/$backup_date/* images/
+    echo "Copy all images from backup $backup_dir to directory images."
+    cp $backup_dir/* images/
+    echo "Backup is restored!"
+else
+    echo `Directory $backup_dir dont exist.` 
+fi
