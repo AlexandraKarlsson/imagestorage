ebefcc64bc46f73afdf15747bcaa899605d9f1b8
Alexandra Karlsson <betruebefree@gmail.com>
2021-01-17 16:05:41 +0100

    Improved backup and restore scripts.

--- backup.sh
+++ backup.sh
@@ -2,10 +2,18 @@
 
 # Get current date
 current_day=`date +%Y%m%d`
-echo $current_day
+# echo "Current day $current_day"
 
-# Create directory in backup directory
-mkdir backup/$current_day
+backup_dir="backup/$current_day"
+# echo "backup directory $backup_dir"
 
-# Copy all images into the current backup directory
-cp images/* backup/$current_day
+echo "Create backup of all images to the directory $backup_dir."
+
+# TODO: Check if directory already exist and remove it and all files
+
+echo "Create the backup directory $backup_dir."
+mkdir -v $backup_dir
+
+echo "Backup all files in the images directory to $backup_dir."
+cp -v images/* $backup_dir
+echo "Backup is created!"
--- restore.sh
+++ restore.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-# e. g. restore 20210101
+# e. g. ./restore.sh 20210101
 
 backup_date=$1
-echo $backup_date
-backup_dir=`backup/$backup_date/`
-echo $backup_dir
+# echo $backup_date
+backup_dir="backup/$backup_date/"
+# echo $backup_dir
 
-# Check that the backup for the date exists otherwise exit
+echo "Restore all images from the backup directory $backup_dir."
+
+# Check that the backup for the date exists
 if [ -d $backup_dir ] 
 then
-    echo `Directory $backup_dir exists.` 
-    echo "Remove all images in images directory."
-    rm -f images/*
+    echo "Directory $backup_dir exists."
 
-    echo "Copy all images from backup $backup_dir to directory images."
-    cp $backup_dir/* images/
-    echo "Backup is restored!"
+    echo "Remove all images in images directory."
+    rm -v images/*
+
+    echo "Restore all images from backup $backup_dir to images directory."
+    cp -v $backup_dir/* images/
+    echo "Images restored from backup."
 else
-    echo `Directory $backup_dir dont exist.` 
+    echo "Images not restored because $backup_dir does not exist." 
 fi
