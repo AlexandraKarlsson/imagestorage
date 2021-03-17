4b5fb32b1a59ce04f8bda3850b1696e5f92856bf
Alexandra Karlsson <betruebefree@gmail.com>
2021-03-17 11:02:48 +0100

    Added method to rename and remove image/s and use different body encoding as middleware.

--- index.js
+++ index.js
@@ -4,39 +4,65 @@ const ipAddress = getIpAddress()
 const PORT = 8010
 
 const express = require("express")
-const bodyParser = require("body-parser")
 const fs = require("fs")
-const {authenticate} = require("./authenticate")
+const { authenticate } = require("./authenticate")
 const app = express()
 
 
-app.get('/' ,(request,response) => {
-    console.log(`ImageStorage: REST API on IP address ${ipAddress} - GET / ...`)
-    response.send(`ImageStorage: REST API on IP address ${ipAddress} - GET /`)
-  })
+app.get('/', (request, response) => {
+  console.log(`ImageStorage: REST API on IP address ${ipAddress} - GET / ...`)
+  response.send(`ImageStorage: REST API on IP address ${ipAddress} - GET /`)
+})
 
 app.use('/images', express.static('images'))
 
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }))
+const bodyAsJson = express.json()
+const bodyAsUrlencoded = express.urlencoded({ extended: true, limit: "50mb" })
 
 
 //TODO: Fix authentication for tastebudsdelight app
-app.post("/image", /*authenticate,*/ (request, response) => {
+app.post("/image", bodyAsUrlencoded, /*authenticate,*/ (request, response) => {
   var name = request.body.name
   console.log(`POST /image name = ${name}`)
   var image = request.body.image
-  var realFile = Buffer.from(image,"base64")
+  var realFile = Buffer.from(image, "base64")
   fs.writeFile(`images/${name}`, realFile, (error) => {
-      if(error) {
-        console.log(error)
-        response.status(400).send()
-      } else {
-        console.log("OK")
-        response.send("OK")
-      }       
-   })
- })
+    if (error) {
+      console.log(error)
+      response.status(400).send()
+    } else {
+      console.log("OK")
+      response.send("OK")
+    }
+  })
+})
+
+app.put("/image/rename", bodyAsJson, /*authenticate,*/(request, response) => {
+  var oldName = request.body.oldname
+  var newName = request.body.newname
+  console.log(`PUT /image oldName = ${oldName} newName = ${newName}`)
+
+  fs.rename(`images/${oldName}`, `images/${newName}`, (error) => {
+    if (error) {
+      response.status(400).send(error)
+    }
+    response.send(`The image with name ${oldName} was renamed to ${newName} successfully`)
+  })
+})
+
+app.delete("/image/:filename", /*authenticate,*/(request, response) => {
+  var name = request.params.filename
+  console.log(`DELETE /image name = ${name}`)
+  fs.unlink(`images/${name}`, (error) => {
+    if (error) {
+      response.status(400).send(error)
+    }
+    response.send(`The image with name ${name} successfully deleted!`)
+  })
+})
+
+
 
 app.listen(PORT, () => {
-    console.log(`ImageStorage, running on IP address ${ipAddress}, listening on port ${PORT}.`)
+  console.log(`ImageStorage, running on IP address ${ipAddress}, listening on port ${PORT}.`)
 })
